21e2e9c4f2dc90dccb3e149357bf97e75aab23d6
SLAB39 <SLAB39@KM101-14-M>
2025-09-12 11:42:03 +0200

    Ajoutez des fichiers projet.

---
+++ calculatrice/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace calculatrice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        int val1 = 0;
+        int val2 = 0;
+        int result = 0;
+        char operation = ' ';
+
+    }
+}
