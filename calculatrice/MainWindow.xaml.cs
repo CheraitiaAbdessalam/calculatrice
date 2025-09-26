a667c91cf790f8ac90b9b626ce403afbe10f632c
SLAB39 <SLAB39@KM101-14-M>
2025-09-26 11:59:55 +0200

    000

--- calculatrice/MainWindow.xaml.cs
+++ calculatrice/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ namespace calculatrice
         public MainWindow()
         {
             InitializeComponent();
+            
         }
 
         double val1 = 0;
@@ -102,7 +103,7 @@ namespace calculatrice
             TB_Display.Text = result.ToString();
         }
 
-      
+
 
         private void BTN_PlusMinus_Click(object sender, RoutedEventArgs e)
         {
@@ -132,6 +133,64 @@ namespace calculatrice
 
             TB_Display.Text += value;
         }
+
+        private void BTN_Racine_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(TB_Display.Text, out double number))
+            {
+                if (number < 0)
+                {
+                    TB_Display.Text = "Error";
+                    return;
+                }
+                double racine = Math.Sqrt(number);
+                TB_Display.Text = racine.ToString();
+            }
+        }
+
+
+
+        private void BTN_Pourcent_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(TB_Display.Text))
+                return;
+
+            if (double.TryParse(TB_Display.Text, out double val))
+            {
+                // Calcul du pourcentage par rapport à val1 (si val1 est défini)
+                if (operation != ' ' && val1 != 0)
+                {
+                    val = (val1 * val) / 100;
+                    TB_Display.Text = val.ToString();
+                }
+                else
+                {
+                    // Si pas d'opération, affiche simplement val / 100
+                    val = val / 100;
+                    TB_Display.Text = val.ToString();
+                }
+            }
+        }
+
+        private void BTN_cos_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(TB_Display.Text, out double value))
+                TB_Display.Text = Math.Cos(value).ToString();
+        }
+
+        private void BTN_sin_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(TB_Display.Text, out double value))
+                TB_Display.Text = Math.Sin(value).ToString();
+        }
+
+        private void BTN_tan_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(TB_Display.Text, out double value))
+                TB_Display.Text = Math.Tan(value).ToString();
+        }
+
     }
-    }
+}   
+    
 
